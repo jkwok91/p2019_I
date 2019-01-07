ef2d3ba40580ec8dba34e70499c80772df33ad07
Jessica Kwok <jk3405@nyu.edu>
2019-01-06 19:20:45 -0800

    initial commit
    this is a practice run for learning how to use unity again, and also working with the gearvr
    
    video of project on gearvr: https://www.youtube.com/watch?v=dZL2IqJr8mo

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	[SerializeField] private GameObject floor;
+
+	float speed;
+	Camera facing;
+
+	// Use this for initialization
+	void Start () {
+		speed = 1f;
+		facing = GameObject.Find("Main Camera").GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		OVRInput.Update();
+		if (SystemInfo.deviceType == DeviceType.Handheld) {
+			if (OVRInput.Get(OVRInput.Touch.PrimaryTouchpad)) {
+				/*
+				// this is for testing.
+				Renderer rend = floor.GetComponent<Renderer>();
+				rend.material.SetColor("_Color", Color.red);
+				*/
+
+				transform.position = transform.position + facing.transform.forward * Time.deltaTime * speed; 
+			}
+		} else {
+		// technically don't even need this since i'm not putting in mouselook cuz i'm a piece of shit
+			transform.position = transform.position + facing.transform.forward * Time.deltaTime * speed * Input.GetAxis("Vertical");
+		}
+
+	}
+}
---
+++ Assets/Scripts/SetText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetText : MonoBehaviour {
+
+	string deviceType;
+
+	// Use this for initialization
+	void Start () {
+		if (SystemInfo.deviceType == DeviceType.Desktop) {
+			deviceType = "Desktop";
+		}
+		else if (SystemInfo.deviceType == DeviceType.Handheld) {
+			deviceType = "Handheld";
+		}
+		else if (SystemInfo.deviceType == DeviceType.Console) {
+			deviceType = "Console";
+		} else {
+			deviceType = "Unknown";
+		}
+		gameObject.GetComponent<TextMesh>().text = deviceType;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
